e796275bfef69474709c5101dbd4cffef4fa7eb0
Ryan Juza <ryanjuza@gmail.com>
2020-11-22 11:55:30 -0500

    done

---
+++ UdemyWhileLoops/src/UdemyWhileLoops.java
@@ -0,0 +1,13 @@
+
+public class UdemyWhileLoops {
+	public static void main(String[] args) {
+		int value = 0;
+		boolean loop = value < 20;
+		
+		while(value < 10) {
+		
+		System.out.println("value = " + value);
+		value++;
+		}
+	}
+}
