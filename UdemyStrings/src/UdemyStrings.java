bc02b98ad4de2c3c5a55cd1789de9449911b603d
Ryan Juza <ryanjuza@gmail.com>
2020-11-22 11:46:17 -0500

    git isnt committing this java file

---
+++ UdemyStrings/src/UdemyStrings.java
@@ -0,0 +1,14 @@
+
+public class UdemyStrings {
+	public static void main(String[] args) {
+		int myInt = 7;
+		String text = "Hello";
+		String blank = " ";
+		String name = "Bob";
+		String greeting = text+blank+name;
+		
+		System.out.println(greeting);
+		
+		System.out.println("My integer is " + myInt);
+	}
+}
