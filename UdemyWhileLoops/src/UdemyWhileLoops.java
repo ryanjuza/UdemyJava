54bda9aa3bc288449421a1032ddfc2c6e3e42dba
Ryan Juza <ryanjuza@gmail.com>
2020-11-22 16:19:05 -0500

    added do/while to while loop

--- UdemyWhileLoops/src/UdemyWhileLoops.java
+++ UdemyWhileLoops/src/UdemyWhileLoops.java
@@ -1,7 +1,9 @@
+import java.util.Scanner;
 
 public class UdemyWhileLoops {
 	public static void main(String[] args) {
 		
+		//////////while//////////////////////////////////
 		int value = 0;
 		boolean loop = value < 20;
 		
@@ -10,5 +12,27 @@ public class UdemyWhileLoops {
 		System.out.println("value = " + value);
 		value++;
 		}
+		
+		////////do while//////////////////////////////////
+		
+//		//wrong
+//		Scanner scanner = new Scanner(System.in);
+//		int val = scanner.nextInt();
+//		while (val != 5) {
+//			System.out.println("Enter a number: ");
+//			val = scanner.nextInt();
+//			}
+//		System.out.println("Got 5!");
+		
+		//correct, will always run at least once
+		Scanner scanner = new Scanner(System.in);
+		int val = 0;
+		do {
+			System.out.println("Enter a number: ");
+			val = scanner.nextInt();
+		}
+		while (val != 5);
+		
+		System.out.println("Got 5!");
 	}
 }
