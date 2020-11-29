27433eebe0249b741f1f98a916a1b79effb7eded
Ryan Juza <ryanjuza@gmail.com>
2020-11-29 06:47:12 -0500

    getters

--- UdemyClassesAndObjects/src/UdemyClassesAndObjects.java
+++ UdemyClassesAndObjects/src/UdemyClassesAndObjects.java
@@ -6,6 +6,19 @@ class Person{
 	void speak() {
 		System.out.println("Hi, my name is " + name + " and I am " + age + " years old.");
 	}
+	
+	int yearsUntilRetirement() {
+		int yearsLeft = 65 - age;
+		return yearsLeft;
+	}
+	
+	int getAge() {
+		return age;
+	}
+	
+	String getName() {
+		return name;
+	}
 }
 
 
@@ -18,11 +31,19 @@ public class UdemyClassesAndObjects {
 		person1.name = "Dude Guy";
 		person1.age = 32;
 		person1.speak();
+		int years = person1.yearsUntilRetirement();
+		
+		int age = person1.getAge();
+		String name = person1.getName();
+		
+		System.out.println(name + "'s years left until retirement: " + years + ".");
 		
 		Person person2 = new Person();
 		person2.name = "Sally Studebaker";
 		person2.age = 27;
 		person2.speak();
+		
+		
 		
 
 	}
