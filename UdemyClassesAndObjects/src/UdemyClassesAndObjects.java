245ea375fcaf81d9b436706f60e2ef64da5cb588
Ryan Juza <ryanjuza@gmail.com>
2020-11-29 06:25:48 -0500

    Added classes tut

--- UdemyClassesAndObjects/src/UdemyClassesAndObjects.java
+++ UdemyClassesAndObjects/src/UdemyClassesAndObjects.java
@@ -1,9 +1,24 @@
 
+class Person{
+	String name;
+	int age;
+}
+
+
 public class UdemyClassesAndObjects {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Person person1 = new Person();
+		
+		person1.name = "Dude Guy";
+		person1.age = 32;
+		
+		Person person2 = new Person();
+		person1.name = "Sally Studebaker";
+		person1.age = 27;
+		
+		System.out.println(person1.name);
 	}
 
 }
